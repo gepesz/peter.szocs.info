c146a50c2352d75a1019bf74aa39525b69796be3
Peter Szocs <gepesz@gmail.com>
2015-07-31 08:30:07 -0400

    mail

--- contact.php
+++ contact.php
@@ -4,10 +4,15 @@ $errors = array(); // array to hold validation errors
 $data   = array(); // array to pass back data
 
 // locals
-$name = $_POST['name'];
-$email_address = $_POST['email'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
+//$name = $_POST['name'];
+//$email_address = $_POST['email'];
+//$phone = $_POST['phone'];
+//$message = $_POST['message'];
+
+$name = "John Smith";
+$email_address = "somebody@gmail.com";
+$phone = "123-456-7890";
+$message = "Hi Peter,\n\nGood looking website!  I was wondering if I could grab a copy of your resume perhaps?  Please send me a copy.\n\nThanks,\nJohn";
 
 // sanity checks
 if ( empty($name) ) {
@@ -26,13 +31,13 @@ if ( empty($message) ) {
 if ( empty($errors) ) {
      // send email
      $to = "gepesz@hotmail.com";
-     $email_subject = "Website Contact From:  ".$name;
+     $email_subject = "Contact from your website!";
      $email_body = "New message arrived from http://peter.szocs.info.\n\n";
-     $email_body .= "Name:  ".$name."\n\n";
-     $email_body .= "Email: ".$email_address."\n\n";
+     $email_body .= "Name:  ".$name."\n";
+     $email_body .= "Email: ".$email_address."\n";
      $email_body .= "Phone: ".$phone."\n\n";
-     $email_body .= "Message:\n".$message;
-     $headers = "From: noreply@peter.szocs.info\n";
+     $email_body .= $message;
+     $headers = "From: ".$name." <".$email_address.">\n";
      $headers .= "Reply-To: ".$email_address;
      $retval = mail($to, $email_subject, $email_body, $headers);
      
