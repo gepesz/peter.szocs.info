1788701a73eaedaf625f59a13689998437d7f4a3
Peter Szocs <gepesz@hotmail.com>
2015-08-29 23:48:58 +0000

    added grunt support

--- js/app.js
+++ js/app.js
@@ -45,6 +45,7 @@ app.config(function($stateProvider, $urlRouterProvider, $locationProvider, $sceD
 
     // Enable html5 mode
     $locationProvider.html5Mode(true);
+    $locationProvider.hashPrefix("!");
     
     // Allow youtube content
     $sceDelegateProvider.resourceUrlWhitelist([
@@ -116,10 +117,22 @@ app.controller("MainCtrl", function($scope, $http) {
 // GitHub controller
 app.controller("GitHubCtrl", function($scope, $http, GitHubSvc) {
     
+    $scope.repos = [{
+        name: "chat",
+        description: "Short description of the repo",
+        html_url: "https://github.com/gepesz/chat",
+        homepage: "https://github.com/gepesz/chat",
+        screenshot: "https://cloud.githubusercontent.com/assets/12737838/9482758/7ba4804a-4b65-11e5-81cf-ee29039efc39.png",
+        body: "This is the detailed description of the repository.",
+        date: "2015-08-15",
+        licenseName: "Apache License 2.0",
+        licenseUrl: "https://api.github.com/licenses/apache-2.0"
+    }];
+    
     // Get repos
-    GitHubSvc.getReleases().then(function(repos) {
-        $scope.repos = repos;
-    });
+    // GitHubSvc.getReleases().then(function(repos) {
+    //     $scope.repos = repos;
+    // });
     
     // Show repo
     $scope.showRepo = function(repo) {
@@ -149,6 +162,11 @@ app.factory("GitHubSvc", function($q, $http) {
     function getRepoReleases(repoName) {
         return getData(apiUrl + "repos/" + userName + "/" + repoName + "/releases");
     }
+
+    // Get the license of a repo with given name
+    function getRepoLicense(repoName) {
+        return getData(apiUrl + "repos/" + userName + "/" + repoName + "/license");
+    }
     
     // Get repo with given name
     function getRepo(repoName) {
@@ -168,32 +186,42 @@ app.factory("GitHubSvc", function($q, $http) {
             repos.map(function(repo) {
                 if ( repo.homepage ) {
                     // showcase this repo
-                    getRepoReleases(repo.name).then(function(releases) {
-                        releases.map(function(release) {
-                            
-                            // screenshot
-                            var index = release.body.indexOf("(https") + 1;
-                            var screenshot = release.body.substring(index);
-                            index = screenshot.indexOf(".png)") + 4;
-                            screenshot = screenshot.substring(0, index);
-                            
-                            // body
-                            index = release.body.indexOf("\r\n") + 2;
-                            var body = release.body.substring(index);
-
-                            // package it up                            
-                            arr.push({
-                                name: repo.name,
-                                description: repo.description,
-                                html_url: repo.html_url,
-                                homepage: repo.homepage,
-                                screenshot: screenshot,
-                                body: body,
-                                date: release.created_at.slice(0, 10)
+                    getRepoReleases(repo.name)
+                        // 1) repo release
+                        .then(function(releases) {
+                            releases.map(function(release) {
+                                
+                                // screenshot
+                                var index = release.body.indexOf("(https") + 1;
+                                var screenshot = release.body.substring(index);
+                                index = screenshot.indexOf(".png)") + 4;
+                                screenshot = screenshot.substring(0, index);
+                                
+                                // body
+                                index = release.body.indexOf("\r\n") + 2;
+                                var body = release.body.substring(index);
+    
+                                // package it up                            
+                                arr.push({
+                                    name: repo.name,
+                                    description: repo.description,
+                                    html_url: repo.html_url,
+                                    homepage: repo.homepage,
+                                    screenshot: screenshot,
+                                    body: body,
+                                    date: release.created_at.slice(0, 10)
+                                });
                             });
-                        });
-                        dfd.resolve(arr);
-                   });
+                        })
+                        // 2) repo license
+                        .then(getRepoLicense(repo.name)
+                            .then(function(license) {
+                                arr[arr.length - 1].licenseName = license.license.name;
+                                arr[arr.length - 1].licenseUrl = license.license.url;
+                                // console.log(arr[arr.length - 1]);
+                                dfd.resolve(arr);
+                            })
+                        );
                 }
             });
         });
@@ -202,6 +230,7 @@ app.factory("GitHubSvc", function($q, $http) {
     
     return {
         getRepoReleases: getRepoReleases,
+        getRepoLicense: getRepoLicense,
         getRepo: getRepo,
         getRepos: getRepos,
         getReleases: getReleases
