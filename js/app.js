2290223b822e68a7e9c329797620323793ffd4bd
Peter Szocs <gepesz@hotmail.com>
2015-09-05 17:52:00 +0000

    firebase & favicon

--- js/app.js
+++ js/app.js
@@ -11,8 +11,9 @@
  * http://peter.szocs.info
  */
 
-/* globals angular */
-var app = angular.module("app", ["ngMessages", "ui.router"]);
+/* globals angular Firebase */
+var app = angular.module("app", ["ngMessages", "ui.router", "firebase"])
+.constant("FIREBASE_URL", "https://peter-szocs-info.firebaseio.com/");
 
 // App config
 app.config(function($stateProvider, $urlRouterProvider, $locationProvider, $sceDelegateProvider) {
@@ -56,26 +57,15 @@ app.config(function($stateProvider, $urlRouterProvider, $locationProvider, $sceD
 });
 
 // Main controller
-app.controller("MainCtrl", function($scope, $http) {
+app.controller("MainCtrl", function($scope, $http, $firebaseArray, FIREBASE_URL) {
 
     // Achievements
-    $scope.achievements = [
-        { when: "1998 - 2002",    imgUrl: "upenn.jpg",     imgAlt: "College",   title: "Humble Beginnings", text: "Attended college at the University of Pennsylvania in Philadelphia.  Finished with triple Bachelor of Science degrees in Finance (Wharton), Computer Science (Engineering) and Mathematics (College)." },
-        { when: "September 2006", imgUrl: "bloomberg.jpg", imgAlt: "Bloomberg", title: "Joining Bloomberg", text: "After a short gig at a web startup in Washington DC, I joined Bloomberg LP as a Financial Software Developer.  This is where I currently work, as a Technical Lead in Equity Derivatives." },
-        { when: "May 2010",       imgUrl: "nyu.jpg",       imgAlt: "NYU",       title: "Masters from NYU",  text: "While working at Bloomberg I attended NYU's Courant Institute, earning a Master of Science degree in Financial Mathematics.  This program is the de facto #1 quant school in America." },
-        { when: "2011 - 2015",    imgUrl: "soccer.jpg",    imgAlt: "Soccer",    title: "Soccer",            text: "One of my biggest passions in life is soccer.  Not only do I captain and coach teams in New York City, but also have won several cups while making lifetime lasting friendships." }
-    ];
-
+    var achievementsRef = new Firebase(FIREBASE_URL + "/achievements");
+    $scope.achievements = $firebaseArray(achievementsRef);
+    
     // Songs
-    $scope.songs = [
-        // { src: "https://www.youtube.com/embed/ceCGIk6tAxY", artist: "Navino",                        title: "Chillin' Time" },
-        // { src: "https://www.youtube.com/embed/F7Gl-IQY9rw", artist: "Nayo",                          title: "African Girl" },
-        // { src: "https://www.youtube.com/embed/HNN-lCCYYNo", artist: "Bonde da Stronda ft. Mr Catra", title: "Mansão Thug Stronda"},
-
-        // { src: "https://www.youtube.com/embed/XyWED2RD3XY", artist: "Erick Morillo",                 title: "Live Your Life" },
-        // { src: "https://www.youtube.com/embed/jIVqsZxRPEA", artist: "Fergie",                        title: "Hold On (Rockit Edit)" },
-        // { src: "https://www.youtube.com/embed/KKDKAAFL_9E", artist: "R.A.W. ft. Amanda Wilson",      title: "Intoxicated" }
-    ];
+    var songsRef = new Firebase(FIREBASE_URL + "/songs");
+    $scope.songs = $firebaseArray(songsRef);
 
     // Send email
     $scope.sendEmail = function(formData) {
---
+++ js/unused/data.js
@@ -0,0 +1,17 @@
+// $scope.achievements = [
+//     { when: "1998 - 2002",    imgUrl: "upenn.jpg",     imgAlt: "College",   title: "Humble Beginnings", text: "Attended college at the University of Pennsylvania in Philadelphia.  Finished with triple Bachelor of Science degrees in Finance (Wharton), Computer Science (Engineering) and Mathematics (College)." },
+//     { when: "September 2006", imgUrl: "bloomberg.jpg", imgAlt: "Bloomberg", title: "Joining Bloomberg", text: "After a short gig at a web startup in Washington DC, I joined Bloomberg LP as a Financial Software Developer.  This is where I currently work, as a Technical Lead in Equity Derivatives." },
+//     { when: "May 2010",       imgUrl: "nyu.jpg",       imgAlt: "NYU",       title: "Masters from NYU",  text: "While working at Bloomberg I attended NYU's Courant Institute, earning a Master of Science degree in Financial Mathematics.  This program is the de facto #1 quant school in America." },
+//     { when: "2011 - 2015",    imgUrl: "soccer.jpg",    imgAlt: "Soccer",    title: "Soccer",            text: "One of my biggest passions in life is soccer.  Not only do I captain and coach teams in New York City, but also have won several cups while making lifetime lasting friendships." }
+// ];
+// achievementsRef.set($scope.achievements);
+
+// $scope.songs = [
+//     { src: "https://www.youtube.com/embed/ceCGIk6tAxY", artist: "Navino",                        title: "Chillin' Time" },
+//     { src: "https://www.youtube.com/embed/F7Gl-IQY9rw", artist: "Nayo",                          title: "African Girl" },
+//     { src: "https://www.youtube.com/embed/HNN-lCCYYNo", artist: "Bonde da Stronda ft. Mr Catra", title: "Mansão Thug Stronda"},
+//     { src: "https://www.youtube.com/embed/XyWED2RD3XY", artist: "Erick Morillo",                 title: "Live Your Life" },
+//     { src: "https://www.youtube.com/embed/jIVqsZxRPEA", artist: "Fergie",                        title: "Hold On (Rockit Edit)" },
+//     { src: "https://www.youtube.com/embed/KKDKAAFL_9E", artist: "R.A.W. ft. Amanda Wilson",      title: "Intoxicated" }
+// ];
+// songsRef.set($scope.songs);
