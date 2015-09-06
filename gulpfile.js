a8b7c384175ddde5e5268b049b508da70b96b83e
Peter Szocs <gepesz@hotmail.com>
2015-09-06 03:49:44 +0000

    gulp deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,85 @@
 var gulp = require('gulp');
-var plugins = require('gulp-load-plugins')();
+var plugins = require('gulp-load-plugins')({lazy: true});
 var browserSync = require('browser-sync').create();
+var secrets = require('./secrets.json');
 
+// Create banner to insert as text before js & css files
 var pkg = require('./package.json');
 var banner = ['/**',
   ' * <%= pkg.name %> - <%= pkg.description %>',
   ' * @version v<%= pkg.version %>',
-  ' * @link <%= pkg.homepage %>',
   ' * @license <%= pkg.license %>',
   ' */',
   ''].join('\n');
 
+// Path to assets
+var paths = {
+  less: 'css/app.less',
+  html: ['index.html', 'partials/*.html'],
+  js: ['js/main.js', 'js/**/*.js']
+};
+
 // Convert less files into css
 gulp.task('less', function() {
-  return gulp.src(['css/app.less'])
+  return gulp.src(paths.less)
   .pipe(plugins.less())
   .pipe(plugins.minifyCss())
   .pipe(plugins.header(banner, { pkg : pkg } ))
-  .pipe(gulp.dest('prod/'));
+  .pipe(gulp.dest('css/'));
 });
 
 // Concat and uglify javascript files
 gulp.task('js', function() {
-  return gulp.src([
-    'js/app.js', 
-    'js/**/*.js',
-    '!js/unused/*.js'
-  ])
+  return gulp.src(paths.js)
   .pipe(plugins.jshint())
   .pipe(plugins.jshint.reporter('default'))  
-  .pipe(plugins.concat('all.js'))
+  .pipe(plugins.concat('app.js'))
   .pipe(plugins.ngAnnotate())
   .pipe(plugins.uglify())
   .pipe(plugins.header(banner, { pkg : pkg } ))
-  .pipe(gulp.dest('prod/'));
+  .pipe(gulp.dest('js/'));
 });
 
-// Watch less & html files for changes
-gulp.task('serve', ['less'], function() {
-
-    browserSync.init({
-        server: '.'
-    });
-
-    gulp.watch('css/*.less', ['less']);
-    // gulp.watch('partials/*.html').on('change', browserSync.reload);
+// Minify html and replace references to minified javascript files
+gulp.task('html', function() {
+  return gulp.src(paths.html)
+    .pipe(plugins.minifyHtml({ empty: true }))
+    // .pipe(plugins.usemin({
+    //   assetsDir: 'js',
+    //   js: [plugins.uglify(), 'concat']
+    // }))    
+    .pipe(gulp.dest('prod/'));
 });
 
-// Default task
-gulp.task('default', function() {
-  console.log('hi from gulp');
+// Static server
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
 });
+
+// Production deployment task using SSH
+gulp.task('deploy', function() {
+  var gulpSSH = new plugins.ssh({
+    ignoreErrors: false,
+    sshConfig: secrets.config
+  });
+
+  return gulpSSH
+    .shell(['cd /srv/www/peter.szocs.info/public', 'git pull', 'npm install', 'npm update', 'npm postinstall'], {filePath: 'shell.log'})
+    .pipe(gulp.dest('logs'));
+});
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+  gulp.watch(paths.less, ['less']);
+  gulp.watch(paths.js, ['js']);
+});
+
+// Production install task
+gulp.task('production', ['less', 'js', 'html']);
+
+// The default task
+gulp.task('default', ['less', 'watch']);
---
+++ js/main.js
@@ -0,0 +1,266 @@
+/**
+ * main.js
+ * 
+ * Application code for http://peter.szocs.info
+ * For details, see https://github.com/gepesz/peter.szocs.info
+ *
+ * Code released under the Apache 2.0 license.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Copyright 2015, Peter Szocs.
+ * http://peter.szocs.info
+ */
+
+/* globals angular Firebase */
+var app = angular.module("app", ["ngMessages", "ui.router", "firebase"])
+.constant("FIREBASE_URL", "https://peter-szocs-info.firebaseio.com/");
+
+// App config
+app.config(function($stateProvider, $urlRouterProvider, $locationProvider, $sceDelegateProvider) {
+    
+    // Configure routes
+    $urlRouterProvider.otherwise("/404.html");
+    
+    $stateProvider
+    .state("home", {
+        url: "/",
+        views: {
+            "about": {
+                controller: "MainCtrl",
+                templateUrl: "partials/about.html"
+            },
+            "portfolio": {
+                controller: "GitHubCtrl",
+                templateUrl: "partials/portfolio.html"
+            },
+            "music": {
+                controller: "MainCtrl",
+                templateUrl: "partials/music.html"
+            },
+            "contact": {
+                controller: "MainCtrl",
+                templateUrl: "partials/contact.html"
+            }
+        }
+    });
+
+    // Enable html5 mode
+    $locationProvider.html5Mode(true);
+    $locationProvider.hashPrefix("!");
+    
+    // Allow youtube content
+    $sceDelegateProvider.resourceUrlWhitelist([
+        "self",
+        "https://www.youtube.com/**"
+    ]);
+
+});
+
+// Main controller
+app.controller("MainCtrl", function($scope, $http, $firebaseArray, FIREBASE_URL) {
+
+    // Achievements
+    var achievementsRef = new Firebase(FIREBASE_URL + "/achievements");
+    $scope.achievements = $firebaseArray(achievementsRef);
+    
+    // Songs
+    var songsRef = new Firebase(FIREBASE_URL + "/songs");
+    $scope.songs = $firebaseArray(songsRef);
+
+    // Send email
+    $scope.sendEmail = function(formData) {
+        // console.log(formData);
+
+        $http({ method  : "POST",
+                url     : "/mail/contact.php",
+	            data    : $.param(formData),
+	            headers : { "Content-Type": "application/x-www-form-urlencoded" }
+	    }).success(function(data) {
+            // console.log(data);
+            
+            if ( !data.success ) {
+                // error
+                var firstName = formData.name;
+                if ( firstName.indexOf(" ") >= 0 ) {    // check for white space
+                    firstName = firstName.split(" ").slice(0, -1).join(" ");
+                }
+                $scope.contactFormModalTitle = "Oops ...";
+                $scope.contactFormModalBody  = "Sorry " + firstName + ", it seems that my mail server is not responding.  Please try again later!";
+            } else {
+                // success
+                $scope.contactFormModalTitle = "Success";
+                $scope.contactFormModalBody  = "Your message has been sent.";
+            }
+            
+            // clear form
+            $scope.contactForm.$setPristine();
+            $scope.contactForm.$setUntouched();
+            $scope.formData = {};
+            
+            // show modal
+            $("#contactFormModal").modal("show");
+        });
+    };
+
+});
+
+// GitHub controller
+app.controller("GitHubCtrl", function($scope, $http, GitHubSvc) {
+    
+    $scope.repos = [{
+        name: "chat",
+        description: "Short description of the repo",
+        html_url: "https://github.com/gepesz/chat",
+        homepage: "http://cairo.peterszocs.com",
+        screenshot: "https://cloud.githubusercontent.com/assets/12737838/9482758/7ba4804a-4b65-11e5-81cf-ee29039efc39.png",
+        body: "This is the detailed description of the repository.",
+        date: "2015-08-15",
+        licenseName: "Apache License 2.0",
+        licenseUrl: "https://api.github.com/licenses/apache-2.0"
+    }];
+    
+    // Get repos
+    // GitHubSvc.getReleases().then(function(repos) {
+    //     $scope.repos = repos;
+    // });
+    
+    // Show repo
+    $scope.showRepo = function(repo) {
+        $scope.selectedRepo = repo;
+        $("#portfolioModal").modal("show");
+    };
+});
+
+// GitHub service
+app.factory("GitHubSvc", function($q, $http) {
+    
+    // GitHub locals
+    var apiUrl = "https://api.github.com/";
+    var userName = "gepesz";
+
+    // Returns GitHub data from the given url
+    function getData(url) {
+        var dfd = $q.defer();
+        $http.get(url)
+            .then(function(response) {
+                dfd.resolve(response.data);
+            });
+        return dfd.promise;
+    }
+
+    // Get all releases of a repo with given name
+    function getRepoReleases(repoName) {
+        return getData(apiUrl + "repos/" + userName + "/" + repoName + "/releases");
+    }
+
+    // Get the license of a repo with given name
+    function getRepoLicense(repoName) {
+        return getData(apiUrl + "repos/" + userName + "/" + repoName + "/license");
+    }
+    
+    // Get repo with given name
+    function getRepo(repoName) {
+        return getData(apiUrl + "repos/" + userName + "/" + repoName);
+    }
+
+    // Get all repos
+    function getRepos() {
+        return getData(apiUrl + "users/" + userName + "/repos");
+    }
+
+    // Get all releases
+    function getReleases() {
+        var dfd = $q.defer();
+        var arr = [];
+        getRepos().then(function(repos) {
+            repos.map(function(repo) {
+                if ( repo.homepage ) {
+                    // showcase this repo
+                    getRepoReleases(repo.name)
+                        // 1) repo release
+                        .then(function(releases) {
+                            releases.map(function(release) {
+                                
+                                // screenshot
+                                var index = release.body.indexOf("(https") + 1;
+                                var screenshot = release.body.substring(index);
+                                index = screenshot.indexOf(".png)") + 4;
+                                screenshot = screenshot.substring(0, index);
+                                
+                                // body
+                                index = release.body.indexOf("\r\n") + 2;
+                                var body = release.body.substring(index);
+    
+                                // package it up                            
+                                arr.push({
+                                    name: repo.name,
+                                    description: repo.description,
+                                    html_url: repo.html_url,
+                                    homepage: repo.homepage,
+                                    screenshot: screenshot,
+                                    body: body,
+                                    date: release.created_at.slice(0, 10)
+                                });
+                            });
+                        })
+                        // 2) repo license
+                        .then(getRepoLicense(repo.name)
+                            .then(function(license) {
+                                arr[arr.length - 1].licenseName = license.license.name;
+                                arr[arr.length - 1].licenseUrl = license.license.url;
+                                // console.log(arr[arr.length - 1]);
+                                dfd.resolve(arr);
+                            })
+                        );
+                }
+            });
+        });
+        return dfd.promise;
+    }
+    
+    return {
+        getRepoReleases: getRepoReleases,
+        getRepoLicense: getRepoLicense,
+        getRepo: getRepo,
+        getRepos: getRepos,
+        getReleases: getReleases
+    };
+});
+
+// Animated scrolling directive
+// app.directive("scrollOnClick", function() {
+//     return {
+//         restrict: "A",
+//         link: function(scope, $elm, attrs) {
+//             $elm.on("click", function() {
+//                 var expanded = ($(".navbar-collapse").attr("aria-expanded") === "true");
+//                 if ( expanded ) {
+//                     $(".navbar-toggle:visible").click();
+//                 }
+//                 var $target = $(attrs.href);
+//                 $("html, body").animate({
+//                     scrollTop: $target.offset().top
+//                 }, 1000, "easeInOutExpo");
+//             });
+//         }
+//     };
+// });
+
+// jQuery
+$(function() {
+
+    // Close the responsive menu on item click
+    $(".navbar-collapse ul li a").click(function() {
+        $(".navbar-toggle:visible").click();
+    });
+    
+    // Page scrolling feature (requires jQuery easing plugin)
+    $("a.page-scroll").bind("click", function(event) {
+        var $anchor = $(this);
+        $("html, body").stop().animate({
+            scrollTop: $($anchor.attr("href")).offset().top
+        }, 1000, "easeInOutExpo");
+        event.preventDefault();
+    });
+
+});
