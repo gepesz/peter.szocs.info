86f60d17b11c0e3bf82a008e98004e76f295f2f9
Peter Szocs <gepesz@hotmail.com>
2015-09-13 23:16:05 +0000

    added back repos

--- js/main.js
+++ js/main.js
@@ -107,22 +107,22 @@ app.controller("MainCtrl", function($scope, $http, $firebaseArray, FIREBASE_URL)
 // GitHub controller
 app.controller("GitHubCtrl", function($scope, $http, GitHubSvc) {
     
-    $scope.repos = [{
-        name: "chat",
-        description: "Short description of the repo",
-        html_url: "https://github.com/gepesz/chat",
-        homepage: "http://cairo.peterszocs.com",
-        screenshot: "https://cloud.githubusercontent.com/assets/12737838/9482758/7ba4804a-4b65-11e5-81cf-ee29039efc39.png",
-        body: "This is the detailed description of the repository.",
-        date: "2015-08-15",
-        licenseName: "Apache License 2.0",
-        licenseUrl: "https://api.github.com/licenses/apache-2.0"
-    }];
+    // $scope.repos = [{
+    //     name: "chat",
+    //     description: "Short description of the repo",
+    //     html_url: "https://github.com/gepesz/chat",
+    //     homepage: "http://cairo.peterszocs.com",
+    //     screenshot: "https://cloud.githubusercontent.com/assets/12737838/9482758/7ba4804a-4b65-11e5-81cf-ee29039efc39.png",
+    //     body: "This is the detailed description of the repository.",
+    //     date: "2015-08-15",
+    //     licenseName: "Apache License 2.0",
+    //     licenseUrl: "https://api.github.com/licenses/apache-2.0"
+    // }];
     
     // Get repos
-    // GitHubSvc.getReleases().then(function(repos) {
-    //     $scope.repos = repos;
-    // });
+    GitHubSvc.getReleases().then(function(repos) {
+        $scope.repos = repos;
+    });
     
     // Show repo
     $scope.showRepo = function(repo) {
